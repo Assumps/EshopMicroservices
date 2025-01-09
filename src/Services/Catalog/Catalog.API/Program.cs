1af4a0d7268d1ac865cb4fab10d47e8e4c01541e
esoft48 - Joao Vitor Ribeiro Assumpcao <ex179765@g.unicamp.br>
2025-01-08 22:32:44 -0300

    feat: implement docker compose to postgres and inject dependencies to Marten

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,8 +2,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    
+}).UseLightweightSessions();
+
+
 var app = builder.Build();
 
 // Configura the HTTP reuqest pipeline.
+
+app.MapCarter();
 
 app.Run();
