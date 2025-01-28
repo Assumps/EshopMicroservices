f9bf82edf97f02aa1661c8e4c7ff271233ca6daf
esoft48 - Joao Vitor Ribeiro Assumpcao <ex179765@g.unicamp.br>
2025-01-28 03:05:17 -0300

    feat: Crud and global validation Exception

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -16,7 +16,7 @@ public class GetProductByIdQueryHandler
 
         if (product == null)
         {
-            throw new ProductNotFoundException();
+            throw new ProductNotFoundException(query.Id);
         }
 
         return new GetProductByIdResult(product);
